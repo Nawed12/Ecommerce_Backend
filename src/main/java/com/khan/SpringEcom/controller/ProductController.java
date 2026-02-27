85e1d3c7a47b8c37d85a39296039e7f16078ce68
Nawed <nawedhero786@gmail.com>
2026-02-28 02:20:48 +0530

    searchById

--- src/main/java/com/khan/SpringEcom/controller/ProductController.java
+++ src/main/java/com/khan/SpringEcom/controller/ProductController.java
@@ -7,16 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ProductController {
 
     @Autowired
@@ -25,5 +23,16 @@ public class ProductController {
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getAllProduct(){
         return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+    
+    @GetMapping("/products/{id}")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product product = productService.getProductById(id);
+        if(product.getId()>0) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
